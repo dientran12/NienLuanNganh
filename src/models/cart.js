00a78de78923686c725b65f29d3481d07bf29bb1
atoan175 <127372998+atoan175@users.noreply.github.com>
2023-10-03 21:46:53 +0700

    thêm

---
+++ src/models/cart.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Cart extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  CartItem.init({
+    userID: DataTypes.STRING,
+    role_code: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Cart',
+  });
+  return Cart;
+};
