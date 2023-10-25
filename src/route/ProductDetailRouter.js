74e7486b6be97ae2a9da32e527f4b4ed9f746155
Kim Hieu <kimhieu280@gmail.com>
2023-10-25 11:54:15 +0700

    update chuc nang up anh cua san pham lan 2

--- src/route/ProductDetailRouter.js
+++ src/route/ProductDetailRouter.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const upload = require('../public/uploads/multer');
+const path = require('path');
 const productDetailController = require('../controller/productDetailController');
 
 router.post('/createDetail', upload.single('image'), productDetailController.createProductDetail);
 router.get('/all', productDetailController.getAllProductDetails);
-router.get('/:id', productDetailController.getProductDetailById);
+router.get('/:id', express.static(path.join(__dirname, 'public')), productDetailController.getProductDetailById);
 router.put('/:id/update', upload.single('image'), productDetailController.updateProductDetail);
 router.delete('/:id/delete', productDetailController.deleteProductDetailById);
 
