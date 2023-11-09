091f4b4d65971ea499f4f806dc76a06264bbc558
Kim Hieu <kimhieu280@gmail.com>
2023-11-09 13:03:04 +0700

    format lại ngày tháng trong update promoion

--- src/controller/promotionController.js
+++ src/controller/promotionController.js
@@ -32,8 +32,10 @@ const promotionController = {
   updatePromotion: async (req, res) => {
     const { id } = req.params;
     const { name, percentage, description, startDate, endDate } = req.body;
+    const formattedStartDate = moment(startDate, 'DD/MM/YYYY').format('MM/DD/YYYY');
+    const formattedEndDate = moment(endDate, 'DD/MM/YYYY').format('MM/DD/YYYY');
     try {
-      const updatedPromotion = await promotionService.updatePromotion(id, name, percentage, description, startDate, endDate);
+      const updatedPromotion = await promotionService.updatePromotion(id, name, percentage, description,  formattedStartDate, formattedEndDate);
       if (updatedPromotion.success) {
         res.status(200).json(updatedPromotion);
       } else {
