d8504de4ff4c1d4bd77c20b05438753fc1d6bd5f
Kim Hieu <kimhieu280@gmail.com>
2023-11-11 14:43:13 +0700

    thông báo tồn tại khi tạo trùng tên khuyến mãi

--- src/services/promotionService.js
+++ src/services/promotionService.js
@@ -46,8 +46,14 @@ const promotionService = {
         if (new Date(startDate) >= new Date(endDate)) {
           throw new Error("Ngày bắt đầu phải trước ngày kết thúc.");
         }
-        
-        // Tạo mới promotion nếu tất cả đối số hợp lệ
+    
+        // Kiểm tra xem có khuyến mãi nào có cùng tên tồn tại không
+        const existingPromotion = await Promotion.findOne({ where: { name } });
+        if (existingPromotion) {
+          throw new Error("Khuyến mãi đã tồn tại với tên này.");
+        }
+    
+        // Tạo mới promotion nếu tất cả đối số hợp lệ và không có trùng tên
         const newPromotion = await Promotion.create({
           name,
           percentage,
@@ -55,13 +61,14 @@ const promotionService = {
           endDate,
           description,
         });
+    
         console.log('New promotion created:', newPromotion);
         return newPromotion;
       } catch (error) {
         console.error(error.message);
         return { error: error.message }; // Trả về thông báo lỗi
       }
-    },    
+    },       
 
       //createPromotion('Special Sale', 15, new Date(), new Date('2023-12-31'));
       updatePromotion: async (id, name, percentage, description, startDate, endDate) => {
