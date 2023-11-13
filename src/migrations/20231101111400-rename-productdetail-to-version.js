b72c7217e3a3121e6fcc0c691fc9917cd2d4e76c
Kim Hieu <kimhieu280@gmail.com>
2023-11-13 17:54:51 +0700

    thêm lại migration

---
+++ src/migrations/20231101111400-rename-productdetail-to-version.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.renameTable('ProductDetails', 'Versions');
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.renameTable('Versions', 'ProductDetails');
+  }
+};
