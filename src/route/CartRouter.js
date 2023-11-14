28da397395f8061344445a81a7ad5a8a4d84213b
B1906590 <toanb1906590@student.ctu.edu.vn>
2023-11-14 17:15:26 +0700

    them

--- src/route/CartRouter.js
+++ src/route/CartRouter.js
@@ -4,12 +4,22 @@ import express from 'express'
 
 const router = express.Router();
 
-// router.post('/addtocart/:id', CartItemControllers.addtocart)
+// chức năng thêm sản phẩm vào giỏ hàng bằng sizeitemid
 router.post('/addtocart/:userId/:sizeItemId', CartItemControllers.addtocartitem)
+
+// chỉnh sửa giỏ hàng
 router.post('/updatecart/:userId/:sizeItemId', CartItemControllers.updatecart)
+
+//xóa sản phẩm ra khỏi giỏ hàng
 router.delete('/deletecart/:userId/:sizeItemId', CartItemControllers.deletecart)
+
+//xem thông tin chi tiết sản phẩm trong giỏ hàng
 router.get('/getcartitem/:id', CartItemControllers.getCartItem)
+
+//xem thông tin giỏ hàng
 router.get('/getCart/:id', CartControllers.getCart)
+
+//xem giỏ hàng (tất cả sản phẩm trong giỏ hàng)
 router.get('/getalldata/:userId', CartItemControllers.getAllCartItemController)
 
 module.exports = router
--- src/route/OrderRouter.js
+++ src/route/OrderRouter.js
@@ -7,10 +7,19 @@ import { confirmOrder } from '../controller/OrderController';
 import { getTotalForMonth } from '../controller/OrderController';
 const router = express.Router();
 
+//thêm sản phẩm vào đơn hàng bằng sizeiemid
 router.post('/addProduct/:userId', addtoOrder);
+
+//hủy đơn hàng (xóa đơn hàng)
 router.delete('/cancel/:orderId', cancelOrderController);
+
+//thêm sản phẩm từ giỏ hàng bằng cartiemid
 router.post('/moveMultipleFromCartToOrder', moveFromCartToNewOrderController);
+
+//xác nhận đơn hàng và gửi mail xác nhận, cập nhật trạng thái, số lượng sản phẩm còn lại và cập nhật số sản phẩm đã bán
 router.post('/confirmOrder/:orderId', confirmOrder);
+
+//xem số đơn hàng đã bán và tổng giá trị trong tháng
 router.get('/orders/total', getTotalForMonth);
 
 export default router;
